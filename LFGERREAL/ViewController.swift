11c0ad6258e081ed72eb10e9f2d63d66e7de4126
ahartwel <hartwellalex@gmail.com>
2016-01-16 14:00:38 -0500

    Initial Commit

---
+++ LFGERREAL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LFGERREAL
+//
+//  Created by Alex Hartwell on 1/16/16.
+//  Copyright © 2016 SWARM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
